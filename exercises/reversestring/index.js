ff7149b5429a7efee75fb79a7e1d9a773c8c7cec
jennifertakagi <jennitakagi@gmail.com>
2020-09-09 08:58:48 -0300

    feat: add alternatives solutions to reversestrin

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,8 +8,24 @@
     reverse('Greetings!') === '!sgniteerG'
 */
 
+// function reverse(str) {
+//    return str
+//         .split('')
+//         .reverse()
+//         .join('');
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed
+// }
+
 function reverse(str) {
-  return str.split('').reverse().join('')
+  return str.split('').reduce((reversed, character) => character + reversed, '');
 }
 
 module.exports = reverse;
