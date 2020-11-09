b483adea8a8193e899a0582aa6a9f45743d1f1e5
jennifertakagi <jennitakagi@gmail.com>
2020-11-09 15:28:28 -0300

    feat: add solution to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,57 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   let stair = '';
+
+//   while (stair.length < n) {
+//     stair += '#';
+
+//     let totalSpaces = n - stair.length;
+//     let spaces = '';
+
+//     while (spaces.length < totalSpaces) {
+//       spaces += ' ';
+//     }
+
+//     console.log(stair + spaces);
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+
+//   }
+// }
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
